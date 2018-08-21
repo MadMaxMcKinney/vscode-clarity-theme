ab00cd63ca7c2ecf6da12c96aaf28f6c563d4747
Max McKinney <maxgames101@gmail.com>
2018-08-21 11:29:41 -0500

    Init commit

--- index.ts
+++ index.ts
@@ -1,14 +1,15 @@
 import * as path from 'path';
 import { generateTheme, IColorSet } from 'vscode-theme-generator';
 
-const themeName = 'Generated';
+const themeName = 'Dell Clarity';
 const colors = {
   black: '#111111',
   white: '#FFFFFF',
-  blue: '#5555FF',
-  red: '#FF5555',
-  green: '#55FF55',
-  yellow: '#FFFF33'
+  blue: '#007db8',
+  lightBlue: '#cce5f1',
+  red: '#ce1126',
+  green: '#c1d82f',
+  yellow: '#f2af00'
 }
 
 const colorSet: IColorSet = {
@@ -16,9 +17,17 @@ const colorSet: IColorSet = {
     background: colors.black,
     foreground: colors.white,
     color1: colors.blue,
-    color2: colors.red,
-    color3: colors.green,
+    color2: colors.green,
+    color3: colors.lightBlue,
     color4: colors.yellow
+  },
+  overrides: {
+	  'editor.lineHighlightBackground': '#151515',
+	  'editor.lineHighlightBorder': null,
+	  'editorIndentGuide.background': '#111111',
+	  'editorIndentGuide.activeBackground': '#111111',
+	  'editorBracketMatch.border': '#2A2A2A',
+	  'editorBracketMatch.background': '#111111',
   }
 };
 
